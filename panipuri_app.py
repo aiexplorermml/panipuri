670f2a98960cf205e98f1da6b6e30f684ca0ec37
aiexplorermml <156586900+aiexplorermml@users.noreply.github.com>
2025-06-10 19:27:56 +0530

    Update panipuri_app.py

--- panipuri_app.py
+++ panipuri_app.py
@@ -16,7 +16,7 @@ st.markdown("Shoot puris to protect your stall from samosas! 👆🍽️")
 # Load and display the game HTML
 try:
     html = Path("puri.html").read_text()
-    st.components.v1.html(html, width=600, height=600, scrolling=False,layout="centered",)
+    st.components.v1.html(html, width=900, height=600, scrolling=False,layout="centered")
 except:
     st.error("Game file (puri.html) not found in the same directory")
 
